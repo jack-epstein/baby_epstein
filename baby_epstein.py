7be86b1ca2ce7350e596c0a4b80002a9ff67ed89
jte2004 <jte2004@nyu.edu>
2025-02-19 22:44:49 -0500

    update theming

--- baby_epstein.py
+++ baby_epstein.py
@@ -5,12 +5,13 @@ GENDER = "girl"  # change to "boy" if needed
 if "gender" not in st.session_state:
     st.session_state.gender = "None"
 
-if st.session_state.gender == "girl":
-    st.markdown("# :violet[Baby Epstein is a ...]")
-elif st.session_state.gender == "boy":
-    st.markdown("# :blue[Baby Epstein is a ...]")
-else:
-    st.markdown("# Baby Epstein is a ...")
+# if st.session_state.gender == "girl":
+#     st.markdown("# :violet[Baby Epstein is a ...]")
+# elif st.session_state.gender == "boy":
+#     st.markdown("# :blue[Baby Epstein is a ...]")
+# else:
+#     st.markdown("# :black[Baby Epstein is a ...]")
+st.markdown("# :grey[Baby Epstein is a ...]")
 st.markdown("# 🤰🏻")
 
 gender_button = st.button("Click here to see")
@@ -19,6 +20,7 @@ if gender_button:
     st.session_state.gender = GENDER
     if st.session_state.gender == "girl":
         st.markdown("""# :heartpulse: :violet[GIRL!!!!!] :heartpulse:""")
+        st.markdown("""# :heartpulse: GIRL!!!!! :heartpulse:""")
     elif st.session_state.gender == "boy":
         st.markdown("""# :blue_heart: :blue[BOY!!!!!] :blue_heart:""")
     st.balloons()
