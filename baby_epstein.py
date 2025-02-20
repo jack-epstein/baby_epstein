f270cf4b2b2986178346cbee6e40655cb269216b
jte2004 <jte2004@nyu.edu>
2025-02-19 22:51:44 -0500

    remove theming attempt
    The menu option color was giving this away

--- baby_epstein.py
+++ baby_epstein.py
@@ -5,13 +5,12 @@ GENDER = "girl"  # change to "boy" if needed
 if "gender" not in st.session_state:
     st.session_state.gender = "None"
 
-# if st.session_state.gender == "girl":
-#     st.markdown("# :violet[Baby Epstein is a ...]")
-# elif st.session_state.gender == "boy":
-#     st.markdown("# :blue[Baby Epstein is a ...]")
-# else:
-#     st.markdown("# :black[Baby Epstein is a ...]")
-st.markdown("# :grey[Baby Epstein is a ...]")
+if st.session_state.gender == "girl":
+    st.markdown("# :violet[Baby Epstein is a ...]")
+elif st.session_state.gender == "boy":
+    st.markdown("# :blue[Baby Epstein is a ...]")
+else:
+    st.markdown("# :black[Baby Epstein is a ...]")
 st.markdown("# 🤰🏻")
 
 gender_button = st.button(":grey[Click here to see]")
@@ -20,7 +19,6 @@ if gender_button:
     st.session_state.gender = GENDER
     if st.session_state.gender == "girl":
         st.markdown("""# :heartpulse: :violet[GIRL!!!!!] :heartpulse:""")
-        st.markdown("""# :heartpulse: GIRL!!!!! :heartpulse:""")
     elif st.session_state.gender == "boy":
         st.markdown("""# :blue_heart: :blue[BOY!!!!!] :blue_heart:""")
     st.balloons()
